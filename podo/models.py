3e8030d50bb662239336ff930b9b060ab8bdaea6
vitokate <victor.gonzalezs@utem.cl>
2023-11-18 00:41:28 -0300

    add first changes

--- podo/models.py
+++ podo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Podologo(models.Model):
+    nombre = models.CharField(max_length=100)
+    # Otros campos relevantes
+
+    def __str__(self):
+        return self.nombre
+
+class Cita(models.Model):
+    fecha_hora = models.DateTimeField()
+    podologo = models.ForeignKey(Podologo, on_delete=models.CASCADE)
+    cliente = models.CharField(max_length=100)  # O referencia a un modelo Cliente
+
+    def __str__(self):
+        return f"{self.fecha_hora} - {self.podologo}"
