bdaa27a35a1994f8b209c48d4e4325bf02c324dd
vitokate <victor.gonzalezs@utem.cl>
2023-11-18 01:13:26 -0300

    Update forms.py

--- podo/forms.py
+++ podo/forms.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect  # Agregado redirect aquí
 from .models import Cita
 from .forms import CitaForm
 from django.views.generic import ListView
@@ -10,7 +10,10 @@ class CitaListView(ListView):
 
     def get_queryset(self):
         # Aquí puedes modificar el queryset para filtrar por día, mes, año
+        # Por ahora, simplemente devuelve todas las citas
+        return Cita.objects.all()
 
+# Agregamos una indentación adecuada para la función agendar_cita
 def agendar_cita(request):
     if request.method == 'POST':
         form = CitaForm(request.POST)
@@ -19,4 +22,5 @@ def agendar_cita(request):
             return redirect('lista_citas')
     else:
         form = CitaForm()
+
     return render(request, 'podo/agendar_cita.html', {'form': form})
